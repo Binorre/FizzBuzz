67e2ffc0eef09b4f6c3a6e188dcc0c958447f735
Binor <norbertloboda@gmail.com>
2022-09-23 21:42:07 +0200

    Add project files.

---
+++ pracadomowa4/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracadomowa4
+{
+    class FizzBuzz
+    {
+        public string CheckRemainder(int numInt)
+        {
+
+            if (numInt % 5 == 0 && numInt % 3 == 0)
+                return "FizzBuzz";
+
+            if (numInt % 3 == 0)
+                return "Fizz";
+
+            if (numInt % 5 == 0)
+                return "Buzz";
+
+            return numInt.ToString();
+        }
+
+    }
+}
---
+++ pracadomowa4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracadomowa4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            var fizzBuzz = new FizzBuzz();
+            while (true)
+            {
+                var userInitValue = Console.ReadLine();
+                var success = int.TryParse(userInitValue, out int numInt);
+                if (!success)
+                {
+                    Console.WriteLine("Wpisz wartość liczbową");
+                    continue;
+                }
+                Console.WriteLine(fizzBuzz.CheckRemainder(numInt));
+            }
+        }
+    }
+}
